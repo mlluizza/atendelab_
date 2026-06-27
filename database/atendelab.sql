d40b98d6d0ccf1f5007aeb076c2f9cb5bd37a83a
Maria Luiza <ml9634100@gmail.com>
2026-06-27 15:27:49 -0300

    ajuste no seed com hash errada

--- database/atendelab.sql
+++ database/atendelab.sql
@@ -61,7 +61,7 @@ INSERT INTO usuarios (nome, email, senha, perfil, status)
     VALUES (
         'Administrador',
         'admin@atendelab.com',
-        '$2y$10$J9P2kU2BAMZ3TZcuxTsW4e1D/lka8EocYHzvyoOZmCNcWDQz3RuVC',
+        '$2y$10$.x6/GTkVIJcumdxSqpJ7yOCFykh6Q8yjyhyNbsq2IdOshsJbC24Fq',
         'admin',
         'ativo'
     );
